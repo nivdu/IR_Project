fd095dab079baf75f6e84719f5cf4470b708fef2
lorentsa <lorentsa@post.bgu.ac.il>
2018-11-09 15:30:13 +0200

    creating the first law of number in Parse class

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,10 +6,25 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+//        String string = "-1,234.15";
+//        boolean numeric = true;
+//
+//        numeric = string.matches("-?\\d+(\\.\\d+)?");
+//
+//        if(numeric)
+//            System.out.println(string + " is a number");
+//        else
+//            System.out.println(string + " is not a number");
+
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
