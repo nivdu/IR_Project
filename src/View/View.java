ee65a100506f0017aead613a8481325e11f99dca
nivdu <nivdu@post.bgu.ac.il>
2018-12-05 16:03:08 +0200

    model

--- src/View/View.java
+++ src/View/View.java
@@ -2,12 +2,16 @@ package View;
 
 import Controller.Controller;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class View {
@@ -31,6 +35,8 @@ public class View {
     private Button invertIndex;
     @FXML
     private Button reset;
+    @FXML
+    private Button commit;
 
     private Controller controller = new Controller();//todo how to do??????
 
@@ -38,10 +44,67 @@ public class View {
     @FXML
     private void Commit(ActionEvent event) throws IOException {
         String from = pathFrom.getText();
+//        commit.setDisable();
+        if(from==null || from.equals("No Directory selected")||from.equals("")) {
+            Alert chooseFile = new Alert(Alert.AlertType.ERROR);
+            chooseFile.setHeaderText("Error with path from");
+            chooseFile.setContentText("You must choose legal path of directory to load from before commit");
+            chooseFile.show();
+            return;
+        }
+        pathFrom.setDisable(true);
+        BrowseFrom.setDisable(true);
         String to = pathTo.getText();
+        if(to==null || to.equals("No Directory selected")|| to.equals("")) {
+            Alert chooseFile = new Alert(Alert.AlertType.ERROR);
+            chooseFile.setHeaderText("Error with path to");
+            chooseFile.setContentText("You must choose legal path of directory to save to before commit");
+            chooseFile.show();
+            return;
+        }
+        pathTo.setDisable(true);
+        BrowseTo.setDisable(true);
         boolean stem = stemming.isSelected();
         boolean isSucceed = controller.commit(from,to,stem);
+        if(isSucceed) {
+            reset.setDisable(false);
+            load.setDisable(false);
+            display.setDisable(false);
+            pathFrom.setDisable(false);
+            pathTo.setDisable(false);
+            BrowseFrom.setDisable(false);
+            BrowseTo.setDisable(false);
+        }
+        else {
+            pathFrom.setDisable(false);
+            pathTo.setDisable(false);
+            BrowseFrom.setDisable(false);
+            BrowseTo.setDisable(false);
+        }
+    }
 
+    @FXML
+    private void browseFrom(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        File selectedDirectory = directoryChooser.showDialog(stage);
+        if (selectedDirectory == null) {
+            pathFrom.setText("No Directory selected");
+        } else {
+            pathFrom.setText(selectedDirectory.getAbsolutePath());
+        }
+    }
+
+    @FXML
+    private void browseTo(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        File selectedDirectory = directoryChooser.showDialog(stage);
+        if (selectedDirectory == null) {
+            pathTo.setText("No Directory selected");
+        } else {
+            pathTo.setText(selectedDirectory.getAbsolutePath());
+        }
     }
 
     @FXML
