dc25ef15917ed4db65d24f20e68251fdb3913c87
nivdu <nivdu@post.bgu.ac.il>
2018-12-21 00:47:49 +0200

    BM25 func at Ranker

--- src/Model/Query.java
+++ src/Model/Query.java
@@ -1,7 +1,19 @@
 package Model;
 
-public class query {
+import java.util.HashMap;
+
+public class Query {
+    private HashMap<String,Integer> docsLenghsNoStem;
+    private HashMap<String,Integer> docsLenghsStem;
     public String[] getDocSplited() {
         return null;
+    }
+
+    public void setDocsLenghsNoStem(HashMap<String, Integer> docsLenghsNoStem) {
+        this.docsLenghsNoStem = docsLenghsNoStem;
+    }
+
+    public void setDocsLenghsStem(HashMap<String, Integer> docsLenghsStem) {
+        this.docsLenghsStem = docsLenghsStem;
     }
 }
